0247eb38ac5041a78392a3e6bb9f01bc32fb2726
abdullahehab <gitlab39@gmail.com>
2019-05-22 20:45:51 +0200

    User class

--- lib/UI/Pages/UserDetails.dart
+++ lib/UI/Pages/UserDetails.dart
@@ -1,22 +1,35 @@
 class UserDetails {
+  String birth,
+      education,
+      email,
+      jobTitle,
+      name,
+      numberOfParts,
+      numberOfReading,
+      type,
+      photoUrl;
 
-  String birth, education, email, jobTitle, name, numberOfParts, numberOfReading, type, photoUrl;
+  UserDetails(
+      {this.birth,
+      this.education,
+      this.email,
+      this.jobTitle,
+      this.name,
+      this.numberOfParts,
+      this.numberOfReading,
+      this.type,
+      this.photoUrl});
 
-
-  UserDetails({this.birth, this.education, this.email, this.jobTitle, this.name,
-    this.numberOfParts, this.numberOfReading, this.type, this.photoUrl});
-
-  factory UserDetails.fromJson(Map<String, dynamic> json) {
+  factory UserDetails.fromMap(Map<String, dynamic> json) {
     return new UserDetails(
-      birth: json['birth'],
-      education: json['education'],
-      email: json['email'],
-      jobTitle: json['jobTitle'],
-      name: json['name'],
-      numberOfParts: json['numberOfParts'],
-      numberOfReading: json['numberOfReading'],
-      type: json['type'],
-      photoUrl: json['photoUrl']
-    );
+        birth: json['birth'],
+        education: json['education'],
+        email: json['email'],
+        jobTitle: json['jobTitle'],
+        name: json['name'],
+        numberOfParts: json['numberOfParts'],
+        numberOfReading: json['numberOfReading'],
+        type: json['type'],
+        photoUrl: json['photoUrl']);
   }
 }
