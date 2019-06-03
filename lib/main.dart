66d41495591731e795e99a7ed9f2102ffc5dab2e
abdullahehab <gitlab39@gmail.com>
2019-06-03 19:12:44 +0200

    change run page

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:QCSE/UI/Pages/WatsonChatBot.dart';
 import 'package:QCSE/UI/Pages/editProfile.dart';
 import 'package:QCSE/UI/Pages/viewUserProfile.dart';
 import 'package:QCSE/UI/Pages/voiceSearch.dart';
+import 'package:QCSE/UI/SplashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:dcdg/dcdg.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
-  home: logInPage(),
+  home: SplashScreen(),
 ));
